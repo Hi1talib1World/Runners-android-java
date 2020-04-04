4df07b6ef5c1ec5f5b502490658b96a19e405fbe
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-04 06:50:14 +0100

    Create MyData.java

---
+++ app/src/main/java/com/denzo/runners/CardView/MyData.java
@@ -0,0 +1,13 @@
+package com.denzo.runners.CardView;
+
+import com.denzo.runners.R;
+
+public class MyData {
+
+    static String[] nameArray = {"Cupcake", "Donut", "Eclair"};
+    static String[] versionArray = {"1.5", "1.6", "2.0-2.1" };
+
+    static Integer[] drawableArray = {R.drawable.cupcake, R.drawable.donut, R.drawable.eclair};
+
+    static Integer[] id_ = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+}
