ec2ffe52a111c2408239432c178a60aec51dd0c8
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-19 03:35:32 +0000

    updates ywm

--- app/src/main/java/com/denzo/runners/tabs/wFragment.java
+++ app/src/main/java/com/denzo/runners/tabs/wFragment.java
@@ -1,4 +1,29 @@
 package com.denzo.runners.tabs;
 
-public class wFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.denzo.runners.R;
+
+public class wFragment extends Fragment {
+    public wFragment() {
+// Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+// Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_year, container, false);
+    }
+
 }
