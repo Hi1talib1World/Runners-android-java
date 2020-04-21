eaf6ecc3c03a32063b36d4e0bcac90f0023da18e
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-21 02:06:40 +0000

    Update starFragment.java

--- app/src/main/java/com/denzo/runners/ui/star/starFragment.java
+++ app/src/main/java/com/denzo/runners/ui/star/starFragment.java
@@ -9,11 +9,18 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.carto.layers.CartoBaseMapStyle;
+import com.carto.layers.CartoOnlineVectorTileLayer;
+import com.carto.ui.MapView;
 import com.denzo.runners.R;
 import com.denzo.runners.ui.home.HomeViewModel;
 
 public class starFragment extends Fragment {
     private StarViewModel starViewModel;
+    final String LICENSE = "YOUR_LICENSE_KEY";
+
+    private MapView mapView;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         starViewModel =
@@ -21,6 +28,16 @@ public class starFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_star, container, false);
 
 
+
+        // Register the license so that CARTO online services can be used
+        MapView.registerLicense(LICENSE);
+
+        // Get 'mapView' object from the application layout
+        mapView = (MapView) root.findViewById(R.id.mapView);
+
+        // Add basemap layer to mapView
+        CartoOnlineVectorTileLayer baseLayer = new CartoOnlineVectorTileLayer(CartoBaseMapStyle.CARTO_BASEMAP_STYLE_VOYAGER);
+        mapView.getLayers().add(baseLayer);
         return root;
     }
 }
