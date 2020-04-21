307bef72937cfb5bb9e3084b22b5ff63f2e9b8f9
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-21 00:07:21 +0000

    Update wFragment.java

--- app/src/main/java/com/denzo/runners/ui/myjourney/tabs/wFragment.java
+++ app/src/main/java/com/denzo/runners/ui/myjourney/tabs/wFragment.java
@@ -23,7 +23,7 @@ public class wFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_year, container, false);
+        return inflater.inflate(R.layout.fragment_week, container, false);
     }
 
 }
