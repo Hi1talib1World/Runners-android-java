46b34dd8a1809dd7f3454eed1a0fa1eb65c96293
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-04-21 02:05:02 +0000

    Update starFragment.java

--- app/src/main/java/com/denzo/runners/ui/star/starFragment.java
+++ app/src/main/java/com/denzo/runners/ui/star/starFragment.java
@@ -1,4 +1,26 @@
 package com.denzo.runners.ui.star;
 
-public class starFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.denzo.runners.R;
+import com.denzo.runners.ui.home.HomeViewModel;
+
+public class starFragment extends Fragment {
+    private StarViewModel starViewModel;
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        starViewModel =
+                ViewModelProviders.of(this).get(StarViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_star, container, false);
+
+
+        return root;
+    }
 }
