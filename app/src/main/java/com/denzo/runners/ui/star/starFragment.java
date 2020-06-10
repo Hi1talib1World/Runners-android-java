f665f1affc39286e470849c113b986a094648483
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-06-10 15:08:49 +0100

    Update starFragment.java

--- app/src/main/java/com/denzo/runners/ui/star/starFragment.java
+++ app/src/main/java/com/denzo/runners/ui/star/starFragment.java
@@ -7,10 +7,12 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.databinding.DataBindingUtil;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import com.denzo.runners.databinding.ActivityMainBinding;
 
 import com.commit451.modalbottomsheetdialogfragment.ModalBottomSheetDialogFragment;
 import com.commit451.modalbottomsheetdialogfragment.Option;
@@ -31,15 +33,16 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class starFragment extends Fragment  implements OnMapReadyCallback, ModalBottomSheetDialogFragment.Listener {
+public class starFragment extends Fragment  implements OnMapReadyCallback {
     StarViewModel starViewModel;
     static final String TAG = starFragment.class.getSimpleName();
-    ActivityMainBinding bi;
+
 
     FloatingActionButton fab;
 
