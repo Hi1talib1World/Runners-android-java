7f4bcae82392e226c1719140d3a46146f61cf453
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-07-08 17:42:28 +0100

    Update HomeFragment.java

--- app/src/main/java/com/denzo/runners/ui/home/HomeFragment.java
+++ app/src/main/java/com/denzo/runners/ui/home/HomeFragment.java
@@ -1,22 +1,41 @@
 package com.denzo.runners.ui.home;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.denzo.runners.CardView.DataModel;
+import com.denzo.runners.CardView.MyData;
+import com.denzo.runners.MainActivity;
 import com.denzo.runners.R;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+
+    private static RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private static RecyclerView recyclerView;
+    private static ArrayList<DataModel> data;
+    static View.OnClickListener myOnClickListener;
+    private static ArrayList<Integer> removedItems;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,7 +43,82 @@ public class HomeFragment extends Fragment {
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
+        myOnClickListener = new HomeFragment.MyOnClickListener(this);
+
+        recyclerView = (RecyclerView) root.findViewById(R.id.my_recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
 
         return root;
+    }
+
+    private static class MyOnClickListener implements View.OnClickListener {
+
+        private final Context context;
+
+        private MyOnClickListener(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public void onClick(View v) {
+            removeItem(v);
+        }
+
+        private void removeItem(View v) {
+            int selectedItemPosition = recyclerView.getChildPosition(v);
+            RecyclerView.ViewHolder viewHolder
+                    = recyclerView.findViewHolderForPosition(selectedItemPosition);
+            TextView textViewName
+                    = (TextView) viewHolder.itemView.findViewById(R.id.textViewName);
+            String selectedName = (String) textViewName.getText();
+            int selectedItemId = -1;
+            for (int i = 0; i < MyData.nameArray.length; i++) {
+                if (selectedName.equals(MyData.nameArray[i])) {
+                    selectedItemId = MyData.id_[i];
+                }
+            }
+            removedItems.add(selectedItemId);
+            data.remove(selectedItemPosition);
+            adapter.notifyItemRemoved(selectedItemPosition);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.add_item) {
+            //check if any items to add
+            if (removedItems.size() != 0) {
+                addRemovedItemToList();
+            } else {
+                Toast.makeText(this, "Nothing to add", Toast.LENGTH_SHORT).show();
+            }
+        }
+        return true;
+    }
+
+    private void addRemovedItemToList() {
+        int addItemAtListPosition = 3;
+        data.add(addItemAtListPosition, new DataModel(
+                MyData.nameArray[removedItems.get(0)],
+                MyData.versionArray[removedItems.get(0)],
+                MyData.id_[removedItems.get(0)],
+                MyData.drawableArray[removedItems.get(0)]
+        ));
+        adapter.notifyItemInserted(addItemAtListPosition);
+        removedItems.remove(0);
     }
 }
