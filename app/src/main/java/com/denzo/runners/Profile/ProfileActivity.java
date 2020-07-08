8cf4a4d17b6e97272798cca5bd1dfcc575492bcc
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2020-07-08 06:24:57 +0100

    Update ProfileActivity.java

--- app/src/main/java/com/denzo/runners/Profile/ProfileActivity.java
+++ app/src/main/java/com/denzo/runners/Profile/ProfileActivity.java
@@ -3,8 +3,17 @@ package com.denzo.runners.Profile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.denzo.runners.R;
 
 public class ProfileActivity extends AppCompatActivity {
+
+
+    TextView txtname,txtDesc,txtInfo;
+    ImageView Img1;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
