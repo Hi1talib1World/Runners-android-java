0c39eab2ecd219392bdfb44b0a930f02f1beae21
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-06-10 06:26:41 +0100

    feat: update MainApplication logic

--- app/src/main/java/com/denzo/runners/MainApplication.kt
+++ app/src/main/java/com/denzo/runners/MainApplication.kt
@@ -1,7 +1,9 @@
 package com.denzo.runners
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
