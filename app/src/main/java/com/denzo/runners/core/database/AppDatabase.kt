92b7059c357fef97eabf75f457b3e98b9e1d567c
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-06-13 13:55:09 +0100

    feat: update AppDatabase logic

--- app/src/main/java/com/denzo/runners/core/database/AppDatabase.kt
+++ app/src/main/java/com/denzo/runners/core/database/AppDatabase.kt
@@ -3,15 +3,18 @@ package com.denzo.runners.core.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.denzo.runners.data.local.dao.ConfigDao
 import com.denzo.runners.data.local.dao.RunDao
 import com.denzo.runners.data.local.dao.RunningDAO
+import com.denzo.runners.data.local.entities.ConfigEntity
 import com.denzo.runners.data.local.entities.RunEntity
 import com.denzo.runners.data.local.entities.RunTypeConverters
 import com.denzo.runners.data.local.entities.Runningdata
 
-@Database(entities = [Runningdata::class, RunEntity::class], version = 3, exportSchema = false)
+@Database(entities = [Runningdata::class, RunEntity::class, ConfigEntity::class], version = 4, exportSchema = false)
 @TypeConverters(RunTypeConverters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun getRunningdataDAO(): RunningDAO
     abstract fun getRunDao(): RunDao
+    abstract fun getConfigDao(): ConfigDao
 }
