667652470a379904b1deb4d01347b6297df6f005
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-06-28 20:06:21 +0100

    feat: update RemoteMediator logic

--- app/src/main/java/com/denzo/runners/core/database/RemoteMediator.kt
+++ app/src/main/java/com/denzo/runners/core/database/RemoteMediator.kt
@@ -4,16 +4,16 @@ import androidx.paging.ExperimentalPagingApi
 import androidx.paging.LoadType
 import androidx.paging.PagingState
 import androidx.paging.RemoteMediator
-import com.denzo.runners.data.local.entities.Runningdata
+import com.denzo.runners.data.local.entities.RunEntity
 
 @OptIn(ExperimentalPagingApi::class)
 class RunningRemoteMediator(
     private val database: AppDatabase
-) : RemoteMediator<Int, Runningdata>() {
+) : RemoteMediator<Int, RunEntity>() {
 
     override suspend fun load(
         loadType: LoadType,
-        state: PagingState<Int, Runningdata>
+        state: PagingState<Int, RunEntity>
     ): MediatorResult {
         return try {
             // TODO: Implement syncing logic between Cloud API and Room
