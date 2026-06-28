158209d59bb7d8a95fcf7ca899b611bd08390872
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-06-28 20:06:20 +0100

    feat: update AppDatabase logic

--- app/src/main/java/com/denzo/runners/core/database/AppDatabase.kt
+++ app/src/main/java/com/denzo/runners/core/database/AppDatabase.kt
@@ -3,18 +3,29 @@ package com.denzo.runners.core.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.denzo.runners.data.local.dao.ChallengeDao
 import com.denzo.runners.data.local.dao.ConfigDao
+import com.denzo.runners.data.local.dao.GearDao
+import com.denzo.runners.data.local.dao.RouteDao
 import com.denzo.runners.data.local.dao.RunDao
-import com.denzo.runners.data.local.dao.RunningDAO
+import com.denzo.runners.data.local.dao.WorkoutDao
+import com.denzo.runners.data.local.entities.ChallengeEntity
 import com.denzo.runners.data.local.entities.ConfigEntity
+import com.denzo.runners.data.local.entities.GearEntity
+import com.denzo.runners.data.local.entities.RouteEntity
 import com.denzo.runners.data.local.entities.RunEntity
 import com.denzo.runners.data.local.entities.RunTypeConverters
-import com.denzo.runners.data.local.entities.Runningdata
+import com.denzo.runners.data.local.entities.TrainingPlanEntity
+import com.denzo.runners.data.local.entities.WorkoutEntity
+import com.denzo.runners.data.local.entities.WorkoutTypeConverters
 
-@Database(entities = [Runningdata::class, RunEntity::class, ConfigEntity::class], version = 4, exportSchema = false)
-@TypeConverters(RunTypeConverters::class)
+@Database(entities = [RunEntity::class, ConfigEntity::class, RouteEntity::class, GearEntity::class, TrainingPlanEntity::class, WorkoutEntity::class, ChallengeEntity::class], version = 9, exportSchema = false)
+@TypeConverters(RunTypeConverters::class, WorkoutTypeConverters::class)
 abstract class AppDatabase : RoomDatabase() {
-    abstract fun getRunningdataDAO(): RunningDAO
     abstract fun getRunDao(): RunDao
     abstract fun getConfigDao(): ConfigDao
+    abstract fun getRouteDao(): RouteDao
+    abstract fun getGearDao(): GearDao
+    abstract fun getWorkoutDao(): WorkoutDao
+    abstract fun getChallengeDao(): ChallengeDao
 }
