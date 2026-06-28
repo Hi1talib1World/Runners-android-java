52b906c264fe1813d6b8c6404b341d57dec26580
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2026-06-28 20:06:18 +0100

    feat: update MainApplication logic

--- app/src/main/java/com/denzo/runners/MainApplication.kt
+++ app/src/main/java/com/denzo/runners/MainApplication.kt
@@ -1,11 +1,27 @@
 package com.denzo.runners
 
 import android.app.Application
+import com.denzo.runners.data.local.dao.ChallengeDao
+import com.denzo.runners.data.repository.WorkoutRepository
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltAndroidApp
 class MainApplication : Application() {
+
+    @Inject
+    lateinit var workoutRepository: WorkoutRepository
+
+    @Inject
+    lateinit var challengeDao: ChallengeDao
+
     override fun onCreate() {
         super.onCreate()
+        
+        GlobalScope.launch {
+            workoutRepository.seedMockData(challengeDao)
+        }
     }
 }
